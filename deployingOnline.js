dc9194e1f4a8aecc8ac2ab4f42863b4564a273b2
rinjumanandhar <you@example.com>
2018-11-13 17:11:31 +0545

    nodejs basics gitignore file test

---
+++ deployingOnline.js
@@ -0,0 +1,8 @@
+const http = require('http');
+http.createServer(function(request, response) {
+    // your server code
+   }).listen(process.env.PORT);
+   
+//    http.createServer(function(request, response) {
+//     // your server code
+//    }).listen(process.env.PORT || 3000);
